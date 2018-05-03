f3d7768d6c1cffb15fa7e8dd1d472f6cc7b95399
Bruno <Bruno@DESKTOP-2DD5UCT>
2018-05-03 14:40:23 +0200

    ADDING UI

---
+++ homeland/src/main/java/com/homeland/models/UserToken.java
@@ -0,0 +1,36 @@
+package com.homeland.models;
+
+import com.homeland.dto.UserDTO;
+
+public class UserToken {
+
+	UserDTO user;
+	String token;
+	
+	
+	public UserToken() {}
+	
+	
+	public UserToken(UserDTO user, String token) {
+		super();
+		this.user = user;
+		this.token = token;
+	}
+	
+	
+	public UserDTO getUser() {
+		return user;
+	}
+	public void setUser(UserDTO user) {
+		this.user = user;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+	
+}
