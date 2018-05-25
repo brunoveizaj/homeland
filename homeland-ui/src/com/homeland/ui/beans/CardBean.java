66a207920916f2d87a34508fc795a992fda09e76
Bruno <Bruno@DESKTOP-2DD5UCT>
2018-05-25 09:59:57 +0200

    Fix cardBean

--- homeland-ui/src/com/homeland/ui/beans/CardBean.java
+++ homeland-ui/src/com/homeland/ui/beans/CardBean.java
@@ -7,7 +7,6 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
-<<<<<<< HEAD
 import com.homeland.ui.api.security.ApiException;
 import com.homeland.ui.constants.IDocument;
 import com.homeland.ui.criterias.DocumentRequest;
@@ -92,64 +91,6 @@ public class CardBean implements Serializable{
 	{
 		init();
 	}
-=======
-import com.homeland.ui.criterias.DocumentRequest;
-import com.homeland.ui.models.CardDTO;
-import com.homeland.ui.services.DocumentService;
 
-
-@ManagedBean
-@ViewScoped
-public class CardBean implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-	
-	DocumentRequest request;
-	List<CardDTO> cards;
-	
-
-	public DocumentRequest getRequest() {
-		return request;
-	}
-
-	public void setRequest(DocumentRequest request) {
-		this.request = request;
-	}
-
-	public List<CardDTO> getCards() {
-		return cards;
-	}
-
-	public void setCards(List<CardDTO> cards) {
-		this.cards = cards;
-	}
-	
-	
-	
-	@PostConstruct
-	public void load()
-	{
-		init();
-	}
-	
-	public void init()
-	{
-		this.request = new DocumentRequest();
-		this.cards = null;
-	}
-	
-	public void search()
-	{
-		this.cards = new DocumentService().searchCards(request);
-	}
-	
-	public void clear()
-	{
-		init();
-	}
-	
->>>>>>> refs/remotes/origin/master
-	
-	
 
 }
